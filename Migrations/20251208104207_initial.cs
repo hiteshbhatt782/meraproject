0c09344ae163dfc80c0d7d72cc29ac5766833bd6
Rahul-Bhatt03 <rahulbhatt.fs@gmail.com>
2025-12-08 17:09:05 +0545

    Add project files.

---
+++ Migrations/20251208104207_initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.VisualBasic;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace meraproject.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudentName = table.Column<string>(type: "text", nullable: false),
+                    course = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace meraproject.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name ="Student Id")]
+
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name="Student Name")]
+        public string StudentName { get; set; }
+        public string? course { get; set; }
+        public string email { get; set; }
+    }
+}
